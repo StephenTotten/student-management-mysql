ce9f051ed2acd2434052148bcca3b6945f8ee04b
Totten <stephentotten@presidio.com>
2023-09-24 17:29:55 -0500

    added sequelize

--- index.js
+++ index.js
@@ -1,251 +1,196 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 const readline = require('readline');
-const Student = require('./student');
-const studentList = [];
 
-const dbConfig = {
+// Define the Sequelize model for the Student entity
+const sequelize = new Sequelize('student_management_db', 'root', 'AngryCuddle5', {
   host: 'localhost',
-  user: 'root',
-  password: 'AngryCuddle5',
-  database: 'student_management_db'
-}
-
-async function askQuestion(rl, question) {
-    return new Promise((resolve) => {
-        rl.question(question, resolve);
-    });
-}
-
-const connection = mysql.createConnection(dbConfig);
-
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL: ' + err.message);
-    return;
-  }
-  console.log('Connected to MySQL');
-  main();
+  dialect: 'mysql',
 });
 
-async function main() {
-  console.log('Welcome to Student Management System');
-  await loadStudentsFromDatabase();
-  await promptUser();
-}
+const Student = sequelize.define('student', {
+  name: Sequelize.STRING,
+  age: Sequelize.INTEGER,
+}, {
+  timestamps: false, // Disable timestamps
+});
+
+// Define the readline interface
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 async function promptUser() {
-    console.log('Press the option number to perform the action');
-    console.log('1. Add student');
-    console.log('2. Delete student');
-    console.log('3. Update student');
-    console.log('4. Search student');
-    console.log('5. Print all students');
-    console.log('6. Exit');
+  console.log('Press the option number to perform the action');
+  console.log('1. Add student');
+  console.log('2. Delete student');
+  console.log('3. Update student');
+  console.log('4. Search student');
+  console.log('5. Print all students');
+  console.log('6. Exit');
 
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
+  const choice = await askQuestion('Enter your choice: ');
 
-    const choice = await askQuestion(rl, 'Enter your choice: ');
+  switch (parseInt(choice)) {
+    case 1:
+      // Add student
+      const name = await askQuestion('Enter student name: ');
+      const age = parseInt(await askQuestion('Enter student age: '));
+      await addStudent(name, age);
+      await promptUser();
+      break;
 
-    switch (parseInt(choice)) {
+    case 2:
+      // Delete student
+      const deleteName = await askQuestion('Enter student name to delete: ');
+      await deleteStudent(deleteName);
+      await promptUser();
+      break;
+
+    case 3:
+      // Update student
+      const oldName = await askQuestion('Enter student name to update: ');
+      const newName = await askQuestion('Enter new name: ');
+      const newAge = parseInt(await askQuestion('Enter new age: '));
+      await updateStudent(oldName, newName, newAge);
+      await promptUser();
+      break;
+
+    case 4:
+      // Search student by name or ID
+      console.log('Search by:');
+      console.log('1. Name');
+      console.log('2. ID');
+      const searchOption = await askQuestion('Enter your choice: ');
+
+      switch (parseInt(searchOption)) {
         case 1:
-            // Add student
-            const name = await askQuestion(rl, 'Enter student name: ');
-            const age = parseInt(await askQuestion(rl, 'Enter student age: '));
-            addStudent(name, age);
-            rl.close();
-            await promptUser();
-            break;
+          // Search by name
+          const searchName = await askQuestion('Enter student name to search: ');
+          await searchStudentByName(searchName);
+          await promptUser();
+          break;
 
         case 2:
-            // Delete student
-            const deleteName = await askQuestion(rl, 'Enter student name to delete: ');
-            deleteStudent(studentList, deleteName);
-            rl.close();
-            await promptUser();
-            break;
-
-        case 3:
-            // Update student
-            const oldName = await askQuestion(rl, 'Enter student name to update: ');
-            const newName = await askQuestion(rl, 'Enter new name: ');
-            const newAge = parseInt(await askQuestion(rl, 'Enter new age: '));
-            updateStudent(studentList, oldName, newName, newAge);
-            rl.close();
-            await promptUser();
-            break;
-
-        case 4:
-            // Search student by name or ID
-            console.log('Search by:');
-            console.log('1. Name');
-            console.log('2. ID');
-            const searchOption = await askQuestion(rl, 'Enter your choice: ');
-
-            switch (parseInt(searchOption)) {
-                case 1:
-                    // Search by name
-                    const searchName = await askQuestion(rl, 'Enter student name to search: ');
-                    searchStudentByName(studentList, searchName);
-                    rl.close();
-                    await promptUser();
-                    break;
-
-                case 2:
-                    // Search by ID
-                    const searchID = parseInt(await askQuestion(rl, 'Enter student ID to search: '));
-                    searchStudentByID(studentList, searchID);
-                    rl.close();
-                    await promptUser();
-                    break;
-
-                default:
-                    console.log('Invalid search option. Please try again.');
-                    rl.close();
-                    await promptUser();
-                    break;
-            }
-            break;
-
-        case 5:
-            // Print all students
-            printStudents(studentList);
-            rl.close();
-            await promptUser();
-            break;
-
-        case 6:
-            // Exit and save to file
-            //await saveStudentsToDatabase();
-            console.log('Exiting...');
-            rl.close();
-            process.exit();
+          // Search by ID
+          const searchID = parseInt(await askQuestion('Enter student ID to search: '));
+          await searchStudentByID(searchID);
+          await promptUser();
+          break;
 
         default:
-            console.log('Invalid choice. Please try again.');
-            rl.close();
-            await promptUser();
-            break;
-    }
-}
-
-async function loadStudentsFromDatabase() {
-  try {
-    studentList.length = 0; // Clear the existing studentList
-    const query = 'SELECT * FROM student';
-    connection.query(query, (err, results) => {
-      if (err) {
-        console.error('Error loading students from database: ' + err.message);
-        return;
-      }
-      results.forEach((row) => {
-        const student = new Student(row.name, row.age, row.id);
-        studentList.push(student);
-      });
-    });
-  } catch (err) {
-    console.error('Error loading students from database: ' + err.message);
-  }
-}
-
-function addStudent(name, age) {
-    const query = 'INSERT INTO student (name, age) VALUES (?, ?)';
-    connection.query(query, [name, age], (err, result) => {
-      if (err) {
-        console.error('Error adding student to database: ' + err.message);
-        return;
-      }
-  
-      const newStudent = new Student(name, age, result.insertId); // Capture the auto-generated ID
-  
-      // Only push the student into the array after a successful database insertion
-      studentList.push(newStudent);
-  
-      console.log('Student added to database with ID: ' + result.insertId);
-    });
-  }
-  
-  async function updateStudent(studentList, oldName, newName, newAge) {
-    const query = 'UPDATE student SET name = ?, age = ? WHERE name = ?';
-    connection.query(query, [newName, newAge, oldName], (err, result) => {
-      if (err) {
-        console.error('Error updating student in database: ' + err.message);
-        return;
-      }
-  
-      // Update the local studentList only if the database update succeeds
-      for (const student of studentList) {
-        if (student.name === oldName) {
-          student.name = newName;
-          student.age = newAge;
+          console.log('Invalid search option. Please try again.');
+          await promptUser();
           break;
-        }
       }
-  
-      console.log('Student updated in database.');
-    });
+      break;
+
+    case 5:
+      // Print all students
+      await printStudents();
+      await promptUser();
+      break;
+
+    case 6:
+      // Exit
+      console.log('Exiting...');
+      process.exit();
+
+    default:
+      console.log('Invalid choice. Please try again.');
+      await promptUser();
+      break;
   }
-  
-  function deleteStudent(studentList, deleteName) {
-    const query = 'DELETE FROM student WHERE name = ?';
-    connection.query(query, [deleteName], (err, result) => {
-      if (err) {
-        console.error('Error deleting student from database: ' + err.message);
-        return;
-      }
-  
-      if (result.affectedRows === 0) {
-        console.log('Student not found');
-        return;
-      }
-  
-      // Update the local studentList only if the database delete operation succeeds
-      const updatedStudentList = studentList.filter(student => student.name !== deleteName);
-      studentList.length = 0; // Clear the existing studentList
-      Array.prototype.push.apply(studentList, updatedStudentList); // Copy the updated list back
-  
-      console.log('Student deleted from database.');
-    });
-  }
-
-function printStudents(studentList) {
-    for (const student of studentList) {
-        console.log(`Name: ${student.name} // Age: ${student.age} // ID: ${student.id}`);
-    }
 }
 
-function searchStudentByID(studentList, id) {
-    const foundStudent = studentList.find(student => student.id === id);
-    if (foundStudent) {
-        console.log(`Student found: ${foundStudent.name} // Age: ${foundStudent.age}`);
-    } else {
-        console.log('Student not found');
-    }
+async function askQuestion(question) {
+  return new Promise((resolve) => {
+    rl.question(question, resolve);
+  });
 }
 
-function searchStudentByName(studentList, name) {
-    const foundStudent = studentList.find(student => student.name === name);
-    if (foundStudent) {
-        console.log(`Student found: ${foundStudent.name}, Age: ${foundStudent.age}, ID: ${foundStudent.id}`);
-    } else {
-        console.log('Student not found');
-    }
-}
-
-async function saveStudentsToDatabase() {
+async function addStudent(name, age) {
   try {
-    const values = studentList.map((student) => [student.name, student.age]);
-    const query = 'INSERT INTO student (name, age) VALUES ?';
-    connection.query(query, [values], (err) => {
-      if (err) {
-        console.error('Error saving students to database: ' + err.message);
-        return;
-      }
-      console.log('Changes saved to database');
+    const student = await Student.create({ name, age });
+    console.log('Student added to database with ID: ' + student.id);
+  } catch (err) {
+    console.error('Error adding student to database: ' + err.message);
+  }
+}
+
+async function updateStudent(oldName, newName, newAge) {
+  try {
+    const [updatedRows] = await Student.update({ name: newName, age: newAge }, { where: { name: oldName } });
+    if (updatedRows > 0) {
+      console.log('Student updated in database.');
+    } else {
+      console.log('Student not found.');
+    }
+  } catch (err) {
+    console.error('Error updating student in database: ' + err.message);
+  }
+}
+
+async function deleteStudent(name) {
+  try {
+    const deletedRows = await Student.destroy({ where: { name } });
+    if (deletedRows > 0) {
+      console.log('Student deleted from database.');
+    } else {
+      console.log('Student not found.');
+    }
+  } catch (err) {
+    console.error('Error deleting student from database: ' + err.message);
+  }
+}
+
+async function printStudents() {
+  try {
+    const students = await Student.findAll();
+    students.forEach((student) => {
+      console.log(`Name: ${student.name} // Age: ${student.age} // ID: ${student.id}`);
     });
   } catch (err) {
-    console.error('Error saving students to database: ' + err.message);
+    console.error('Error fetching students from database: ' + err.message);
   }
 }
+
+async function searchStudentByID(id) {
+  try {
+    const student = await Student.findByPk(id);
+    if (student) {
+      console.log(`Student found: ${student.name} // Age: ${student.age}`);
+    } else {
+      console.log('Student not found.');
+    }
+  } catch (err) {
+    console.error('Error searching for student by ID: ' + err.message);
+  }
+}
+
+async function searchStudentByName(name) {
+  try {
+    const students = await Student.findAll({ where: { name } });
+    if (students.length > 0) {
+      students.forEach((student) => {
+        console.log(`Student found: ${student.name}, Age: ${student.age}, ID: ${student.id}`);
+      });
+    } else {
+      console.log('Student not found.');
+    }
+  } catch (err) {
+    console.error('Error searching for student by name: ' + err.message);
+  }
+}
+
+// Sync the Sequelize model with the database and start the application
+sequelize
+  .sync()
+  .then(() => {
+    console.log('Connected to MySQL');
+    console.log('Welcome to Student Management System');
+    promptUser();
+  })
+  .catch((err) => {
+    console.error('Error connecting to MySQL: ' + err.message);
+  });
